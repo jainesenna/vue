0d76302fe207cdb3b9de58a05376325ecbc8d24c
Jaine Senna <jainesantossenna191@gmail.com>
2019-10-14 09:17:49 -0300

    Atualizando

--- nome_projeto/src/App.vue
+++ nome_projeto/src/App.vue
@@ -1,33 +1,62 @@
 <template>
-  <div id="app">
-    <Conteudo/>
-    <SegundoConteudo/>
-    <TerceiroConteudo/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item @click="">
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="">
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <Conteudo />
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import Conteudo from './components/Conteudo.vue'
-import SegundoConteudo from './components/SegundoConteudo.vue'
-import TerceiroConteudo from './components/TerceiroConteudo.vue'
-
-export default {
-  name: 'app',
-  components: {
-    Conteudo,
-    SegundoConteudo,
-    TerceiroConteudo
+import Conteudo from './components/Conteudo'
+  export default {
+    components: {
+      Conteudo
+    },
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+    }),
+    created () {
+      this.$vuetify.theme.dark = true
+    },
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
